c8c0ac759520c36ef1b0984269c69d280d9a3765
Azamat Truscott <32041997+azamattruscott@users.noreply.github.com>
2020-03-19 22:06:48 +0000

    Loads of updates - mainly to clear up the navbar

--- JS/java.js
+++ JS/java.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const navLinksA = document.querySelector(".nav-links-A");
+const navLinksB = document.querySelector(".nav-links-B");
+const links = document.querySelectorAll(".nav-links-A li");
 
 hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
+  navLinksA.classList.toggle("open");
+  navLinksB.classList.toggle("open");
   links.forEach(link => {
     link.classList.toggle("fade");
   });
